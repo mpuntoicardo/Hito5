2fb7836da7b46e6117903b8f42c6b38e898b2746
Marcos <56072621+M4RCKIT0S@users.noreply.github.com>
2019-12-03 20:53:28 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
+const bookRoutes = require('./routes/bookRoutes');
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', userRoutes);
+app.use('/', bookRoutes);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = require('./app');
 
 const mongodb = process.env.MONGODB || 'mongodb://localhost:27017/basicAPI';
 const port = process.env.PORT || 3000;
-
 mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
   if (err) console.log(`ERROR: connecting to Database. ${err}`);
   else app.listen(port, console.log(`API started on: http://localhost:${port}`));
